77e198fed20b4e49e96342cb3fcbf7fc3cf36d09
mjaliz <mrph14@gmail.com>
2024-09-24 12:40:37 +0330

    stop command added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import pytz
 import logging
 import random
+from datetime import tzinfo, datetime
 
 from telegram import (
     KeyboardButton,
@@ -19,6 +21,7 @@ from telegram.ext import (
     PollAnswerHandler,
     PollHandler,
     filters,
+    Defaults,
 )
 import pandas as pd
 
@@ -36,9 +39,18 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-chat_id = 117288777
 df = pd.read_csv("telegram_quizzes.csv")
 df["is_published"] = False
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
 
 
 async def quiz(context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -50,7 +62,7 @@ async def quiz(context: ContextTypes.DEFAULT_TYPE) -> None:
     options = [op.strip() for op in options]
     correct_choice_index = options.index(question.get("correct_choice"))
     await context.bot.send_poll(
-        chat_id=chat_id,
+        chat_id=os.getenv("CHAT_ID"),
         question=question.get("question_text"),
         options=options,
         type=Poll.QUIZ,
@@ -60,17 +72,32 @@ async def quiz(context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def create_quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_message.chat_id
+    text = "Quiz successfully started"
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    if job_removed:
+        text += " and the old one removed"
     context.job_queue.run_custom(
         callback=quiz,
         job_kwargs={
             "trigger": "cron",
             "day": "*",
-            "hour": "8-22",
-            "minute": "1",
+            "hour": "6-23",
+            "minute": "59",
             "second": "1",
             "month": "*",
+            "timezone": pytz.timezone("Asia/Tehran"),
         },
     )
+    await update.effective_message.reply_text(text)
+
+
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = "Quiz successfully cancelled!" if job_removed else "You have no active Quiz."
+    await update.message.reply_text(text)
 
 
 def main() -> None:
@@ -78,6 +105,7 @@ def main() -> None:
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(os.getenv("TOKEN")).build()
     application.add_handler(CommandHandler("quiz", create_quiz))
+    application.add_handler(CommandHandler("stop", stop))
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
